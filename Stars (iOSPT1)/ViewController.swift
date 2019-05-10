34e3386301e7c6df411ab934d30c34dc83c324f7
patelpra337 <patelpra337@gmail.com>
2019-05-09 20:38:36 -0500

    New Stars Project

---
+++ Stars (iOSPT1)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars (iOSPT1)
+//
+//  Created by patelpra on 5/9/19.
+//  Copyright © 2019 Pravin Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
