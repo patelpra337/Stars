9849136ebafb82c6b67df0731b8a2d5e81b2e21c
patelpra337 <patelpra337@gmail.com>
2019-05-12 00:07:14 -0500

    Add comments to controller files

--- Stars (iOSPT1)/StarController.swift
+++ Stars (iOSPT1)/StarController.swift
@@ -10,14 +10,16 @@ import Foundation
 
 class StarController {
     
+    // 14.  We need a initializer to run the func so that it can load from disk
     init() {
         self.loadFromPersistentStore()
     }
     
-    // Source of all truth
+    // 1. Source of all truth, create the array of Star of which is needed by Encapsulation: protect our data from (external) modification
     
     private(set) var stars: [Star] = []
     
+    // 11.  Create a private var to protect only accessible to this code file
     private var persistentFileURL: URL? {
         let fileManager = FileManager.default
         guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
@@ -27,14 +29,18 @@ class StarController {
     return documentsDirectory.appendingPathComponent("stars.plist")
     // /Users/patelpra/Documents/stars.plist
 }
+    // 2.  Now create func addStar to add new stars that will include name, distance (named: "Sun", withDistance: 0.0)
+    // 3.  Create a let constant --> star
+    // 4.  Add the star to the list by appending to the array
     func addStar(named name: String, withDistance distance: Double) {
         let star = Star(name: name, distance: distance)
         stars.append(star)
+        // 12.  Now we need to save the data
         saveToPersistentStore()
     }
     
-    
-    // Saving
+    // Save to disk
+    // 10.  We are now creating two methods --> save to disk and load from disk using basic persistance.  Create guard let and use the persistentFileURL.
     func saveToPersistentStore() {
         guard let url = self.persistentFileURL else { return }
         
@@ -43,11 +49,11 @@ class StarController {
             let data = try encoder.encode(stars)
             try data.write(to: url)
         } catch {
-            NSLog("Error savind stars data: \(error)")
+            NSLog("Error saving stars data: \(error)")
         }
     }
     
-    // Loading
+    // 13.  Load from disk
     func loadFromPersistentStore() {
         let fileManager = FileManager.default
         guard let url = persistentFileURL,
--- Stars (iOSPT1)/ViewController.swift
+++ Stars (iOSPT1)/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 16. Load all the stars as the app starts up, Call the method --> listStars
         self.listStars()
+        // 19. Load the TextFields, Call the method --> clearInput
         self.clearInput()
         
     }
@@ -21,17 +23,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var distanceTextField: UITextField!
     @IBOutlet weak var starsTextView: UITextView!
     
+    // 5.  Create instance variable of which will setup an empty array of [Star] and initialze it
     let starController = StarController()
     
+    // 6.  Next we are going to create a star we need to get the name (String), distance (Double).  Text field text will always be a String and we will need a conversion by using guard let statement.  Create let constant name, distanceString, & distance.  The last let constant we are converting to a string.  We are using the two of the IBOutlets with the let constant.
     @IBAction func starsTextView(_ sender: Any) {
         guard let name = starNameTextField.text,
             let distanceString = distanceTextField.text,
             let distance = Double(distanceString) else { return }
         
+    // 7.  What we need to do now is, add the starController.addStar button
         self.starController.addStar(named: name, withDistance: distance)
+    // 9.  Call the func listStars
         self.listStars()
     }
     
+    // 8.  List all of the stars we're tracking by creating a func --> Create a string; Append text about the star by creating a for in loop; Display it in textview
     private func listStars() {
         var output = ""
         for star in starController.stars {
@@ -40,7 +47,7 @@ class ViewController: UIViewController {
         
         starsTextView.text = output
     }
-    
+    // 18. Add the TextFields to an empty string to clear it
     private func clearInput() {
         self.starNameTextField.text = ""
         self.distanceTextField.text = ""
